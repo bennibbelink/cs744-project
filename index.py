4dd52666eb2ec7af127ca0970d85c242079bbf28
Ben Nibbelink <bnibbs@vt.edu>
2024-03-10 18:48:12 -0500

    end of call

--- index.py
+++ index.py
@@ -1,14 +1,21 @@
 import faiss
-
+import os
 class Index:
 
-    def __init__(self, index_type, train_data):
+    def __init__(self, index_type, xt, xb, xq, gt):
         try:
-            self.index = faiss.read_index('indexes/' + index_type + '.index')
+            self.index = faiss.read_index('indexes/' + index_type + '.index', faiss.IO_FLAG_MMAP)
         except:
-            self.index = faiss.index_factory(train_data.shape[1], index_type)
+            self.index = faiss.index_factory(xt.shape[1], index_type)
             print(f"Training {index_type} index...")
-            self.index.train(train_data)
+            self.index.train(xt)
+            self.index.add(xb)
             print("Write indexes/" + index_type + ".index")
+            if not os.path.exists('indexes'):
+                os.mkdir('indexes')
             faiss.write_index(self.index, 'indexes/' + index_type + ".index")
 
+        self.xb = xb
+        self.xt = xt
+        self.xq = xq
+        self.gt = gt
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import utils
 import index
+import faiss
+import ctypes
+
 def main():
 
     utils.get_sift()
     xt = utils.fvecs_read("sift/sift_learn.fvecs")
-    ind = index.Index("IVF4096,Flat", xt)
+    xb = utils.fvecs_read("sift/sift_base.fvecs")
+    xq = utils.fvecs_read("sift/sift_query.fvecs")
+    gt = utils.ivecs_read("sift/sift_groundtruth.ivecs")
+    ind = index.Index("IVF4096,Flat", xt, xb, xq, gt)
 
+    index_ivf = faiss.extract_index_ivf(ind.index)
+    distances = []
+    labels = []
+    qids = []
+    rids = []
+    
+    libfaiss = ctypes.cdll.LoadLibrary('libfaiss.so')
+    q = xq[0].ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    print(type(q))
+    libfaiss.search_and_return_centroids(index_ivf, 1, q, distances, labels, qids, rids)
+    
+    faiss.search_and_return_centroids(index_ivf, n=1, xin=q, k=1, 
+                                      distances=distances, labels=labels, query_centroid_ids=qids, result_centroid_ids=rids)
+    ind.index.nprobe = 16
+    D, I = ind.index.search(xq, 1)
+    recall_at_1 = (I[:, :1] == gt[:, :1]).sum() / float(xq.shape[0])
+    print("recall@1: %.3f" % recall_at_1)
+    
 
 if __name__ == "__main__":
     main()
